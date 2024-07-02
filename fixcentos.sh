d1a57d82607fe8bec466d03eeb7852f9a9b85cb8
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2024-07-02 20:36:31 +0700

    update

--- fixcentos.sh
+++ fixcentos.sh
@@ -1,4 +1,6 @@
 cd /etc/yum.repos.d
 mv CentOS-Base.repo CentOS-Base.repo.org
 wget -q -N --no-check-certificate -O CentOS-Base.repo https://raw.githubusercontent.com/qtai2901/haproxy/main/CentOS-Base.repo
+sudo yum clean all
+sudo yum update
 echo "done Phí 5 chục"
