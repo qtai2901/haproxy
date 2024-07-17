26dc661fbed33a4d937f5b0111e5ecae3b87bdf9
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2024-07-17 23:09:05 +0700

    Update fixcentos.sh

--- fixcentos.sh
+++ fixcentos.sh
@@ -1,5 +1,12 @@
 cd /etc/yum.repos.d
-mv CentOS-Base.repo CentOS-Base.repo.org
-wget -q -N --no-check-certificate -O CentOS-Base.repo https://raw.githubusercontent.com/qtai2901/haproxy/main/CentOS-Base.repo
+# mv CentOS-Base.repo CentOS-Base.repo.org
+# wget -q -N --no-check-certificate -O CentOS-Base.repo https://raw.githubusercontent.com/qtai2901/haproxy/main/CentOS-Base.repo
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+
+echo "sslverify=false" >> /etc/yum.conf
 sudo yum clean all
 echo "done Phí 5 chục"
